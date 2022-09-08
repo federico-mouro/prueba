57c38036c4de63ebe7c09be31162a2eea32b01a8
federico-mouro <federicomouro02@gmail.com>
2022-09-08 10:55:39 -0300

    proyecto ML inicio

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require ('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) ); 
+
+app.listen(3000,function() {
+    console.log('FUNCIONA');
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'));
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/login.html'));
+});
